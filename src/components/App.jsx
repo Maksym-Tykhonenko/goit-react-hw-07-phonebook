ad87076c74ce379623501391a2c6ba64bba91f2f
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2023-02-01 16:03:24 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { Loader } from './Loader/Loader';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+
+import { Div, Header, PContact, Section } from './App.styled';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!!!
-    </div>
+    <Div>
+      <Header>Phonebook</Header>
+      <Section>
+        <ContactForm />
+      </Section>
+      <PContact>Contacts</PContact>
+      {isLoading && !error && <Loader />}
+      <Filter />
+      <ContactList />
+    </Div>
   );
 };
